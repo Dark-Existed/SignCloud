c3cb53ad89d34ba5df3c854cabb2bd6c94e0f651
Dark-Existed <c2572388311@gmail.com>
2021-03-20 13:35:43 +0800

    feat: change theme

--- app/src/main/java/com/de/signcloud/signinsignup/WelcomeScreen.kt
+++ app/src/main/java/com/de/signcloud/signinsignup/WelcomeScreen.kt
@@ -1,23 +1,92 @@
 package com.de.signcloud.signinsignup
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.de.signcloud.R
 
 @Composable
 fun WelcomeScreen() {
 
+    var brandingBottom by remember { mutableStateOf(0f) }
+    var showBranding by remember { mutableStateOf(true) }
+    var heightWithBranding by remember { mutableStateOf(0) }
+    val currentOffsetHolder = remember { mutableStateOf(0f) }
+    currentOffsetHolder.value = if (showBranding) 0f else -brandingBottom
+    val currentOffsetHolderDp =
+        with(LocalDensity.current) { currentOffsetHolder.value.toDp() }
+    val heightDp = with(LocalDensity.current) { heightWithBranding.toDp() }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        val offset by animateDpAsState(targetValue = currentOffsetHolderDp)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .brandingPreferredHeight(showBranding, heightDp)
+                .offset(y = offset)
+                .onSizeChanged {
+                    if (showBranding) {
+                        heightWithBranding = it.height
+                    }
+                }
+        ) {
+            Branding(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .onGloballyPositioned {
+                        if (brandingBottom == 0f) {
+                            brandingBottom = it.boundsInParent().bottom
+                        }
+                    }
+            )
+        }
+    }
 }
 
+
+private fun Modifier.brandingPreferredHeight(
+    showBranding: Boolean,
+    heightDp: Dp
+): Modifier {
+    return if (!showBranding) {
+        this
+            .wrapContentHeight(unbounded = true)
+            .height(heightDp)
+    } else {
+        this
+    }
+}
 
 @Composable
 fun Branding(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-
+        Logo(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 76.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.app_tagline),
+            style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .fillMaxWidth()
+        )
     }
 }
 
@@ -37,4 +106,13 @@ private fun Logo(
         modifier = modifier,
         contentDescription = null
     )
+}
+
+@Composable
+private fun SignInCreateAccount() {
+    Column() {
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+//            Text(text = stringResource(id = ))
+        }
+    }
 }
