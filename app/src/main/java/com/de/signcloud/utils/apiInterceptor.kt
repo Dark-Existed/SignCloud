bb0e4281a6ef7cb80ea2d645a9108f6d97578863
Dark-Existed <c2572388311@gmail.com>
2021-04-06 11:33:49 +0800

    feat: add api interceptor

---
+++ app/src/main/java/com/de/signcloud/utils/apiInterceptor.kt
@@ -0,0 +1,18 @@
+package com.de.signcloud.utils
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class apiTokenInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token: String = "get token"
+        return chain.run {
+            proceed(
+                request()
+                    .newBuilder()
+                    .addHeader("token", token)
+                    .build()
+            )
+        }
+    }
+}
